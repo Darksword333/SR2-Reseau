770799ab40df3dac855d321124ee3cac4c618c8b
darksword <mazetgabriel333@gmail.com>
2025-03-20 23:23:21 +0100

    presque finis le v3.1, quelques erreurs subsistes mais c'est bientot finis

--- src/proto_tdd_v3.1_emetteur.c
+++ src/proto_tdd_v3.1_emetteur.c
@@ -1,6 +1,6 @@
 /*************************************************************
-* proto_tdd_v3 -  émetteur                                   *
-* TRANSFERT DE DONNEES  v3                                   *
+* proto_tdd_v3.1 -  émetteur                                 *
+* TRANSFERT DE DONNEES  v3.1                                 *
 *                                                            *
 * Protocole Go-Back-N     ARQ                                *
 *                                                            *
@@ -19,16 +19,15 @@
 int main(int argc, char* argv[]){
     int window = 4;
     if (argc > 2) {
-        printf("Usage: %s <emetteur>\n", argv[0]);
+        printf("Usage: %s <window>\n", argv[0]);
         return 1;
     }
     if (argc == 2) {
-        int val = atoi(argv[1]); 
-        if (val >= SEQ_NUM_SIZE) {
+        window = atoi(argv[1]); 
+        if (window >= SEQ_NUM_SIZE) {
             printf("La taille de la fenêtre doit être inférieure à %d\n", SEQ_NUM_SIZE);
             return 1;
         }
-        window = val;
     }
 
     int curseur = 0, borne_inf = 0;
@@ -58,30 +57,30 @@ int main(int argc, char* argv[]){
             fenetre[curseur] = paquet;
 
             vers_reseau(&paquet);
-            if (borne_inf == curseur)
+            printf("[GAB] J'envoie le paquet %d\n", curseur);
+            if (borne_inf == curseur) // Lancement du temporisateur si c'est le premier paquet de la fenêtre
                 depart_temporisateur(100);
             curseur = inc(SEQ_NUM_SIZE, curseur);
         }
         else {
             evt = attendre();
-            if (evt == -1) { //Paquet Recu
-                de_reseau(&pack);
-                if (verifier_controle(&pack) && dans_fenetre(borne_inf, pack.num_seq, window)) {
-                    for (int i=0; i<pack.lg_info; i++) 
-                        message[i] = pack.info[i];
-                    taille_msg = pack.lg_info;
-                    borne_inf = inc(SEQ_NUM_SIZE, pack.num_seq);
-                    if (borne_inf == curseur)
-                        arret_temporisateur();
+            while((borne_inf-1) != curseur){ // borne_inf-1 car borne_inf peut être égal à curseur
+                if (evt == -1){ //Paquet Reçu
+                    de_reseau(&pack);
+                    printf("[GAB] J'ai reçu le paquet %d\n", pack.num_seq);
+                    if (verifier_controle(&pack) && dans_fenetre(borne_inf, pack.num_seq, window))
+                        borne_inf = inc(SEQ_NUM_SIZE, pack.num_seq);
+                    arret_temporisateur();
                 }
-            }
-            else { //Temporisateur Expiré
+                //Sinon Temporisateur Expiré donc retransmission sans incrémentation de la borne_inf
                 i = borne_inf;
-                depart_temporisateur(100);
-                while (i != curseur) { // Retransmission de la borne_inf a la fin de ma fenêtre donc curseur
+                while ((i-1) != curseur) { // Retransmission de tous les paquets de la fenêtre
+                    printf("[GAB] Je retransmets le paquet %d\n", i);
                     vers_reseau(&fenetre[i]);
                     i = inc(SEQ_NUM_SIZE, i);
                 }
+                depart_temporisateur(100);
+                evt = attendre();
             }
         }
         de_application(message, &taille_msg);
--- src/proto_tdd_v3_recepteur.c
+++ src/proto_tdd_v3_recepteur.c
@@ -2,7 +2,7 @@
 * proto_tdd_v3 -  récepteur                                  *
 * TRANSFERT DE DONNEES  v3                                   *
 *                                                            *
-* Protocole Go-Back-N ARQ                                    *
+* Protocole Go-Back-N                                        *
 *                                                            *
 * MAZET Gabriel                                              *
 **************************************************************/
@@ -19,12 +19,7 @@
 int main(int argc, char* argv[])
 {
     int window = 1;
-    int curseur= 0;
     int borne_inf = 0;
-    paquet_t fenetre[window];
-    for (int i=0; i<window; i++) {
-        fenetre[i].lg_info = 0;
-    }
 
     unsigned char message[MAX_INFO];
     paquet_t paquet, pack;
@@ -39,24 +34,33 @@ int main(int argc, char* argv[])
 
     while ( !fin ) {
         de_reseau(&paquet);
-        if (verifier_controle(&paquet) && dans_fenetre(borne_inf, paquet.num_seq, window)){
+        if (verifier_controle(&paquet) && dans_fenetre(borne_inf, paquet.num_seq, window)){ 
             pack.type = ACK;
             pack.num_seq = paquet.num_seq;
             pack.somme_ctrl = generer_controle(&pack);
             vers_reseau(&pack);
-            fenetre[paquet.num_seq] = paquet;
-            while (borne_inf != curseur && fenetre[curseur].lg_info != 0){ /* tant que j'ai des paquets en avance dans mon buffer fenetre et qu'ils sont ceux que j'attends*/
-                for (int i=0; i<fenetre[curseur].lg_info; i++) {
-                    message[i] = fenetre[curseur].info[i];
-                }
-                fin = vers_application(message, paquet.lg_info);
-                borne_inf = inc(SEQ_NUM_SIZE, borne_inf);
+            borne_inf = inc(SEQ_NUM_SIZE, borne_inf);
+            printf("[GAB] J'acquittes le paquet %d\n", pack.num_seq);
+            for (int i=0; i<paquet.lg_info; i++) {
+                message[i] = paquet.info[i];
             }
+            printf("[GAB] Je transmets le paquet %d à l'application\n", pack.num_seq);
+            fin = vers_application(message, paquet.lg_info);
         }
         else {
-            printf("[TRP] Erreur de somme de controle ou hors séquence.\n");
-            if (pack.type == ACK)
-                vers_reseau(&pack); //envoie du dernier bon ack si il y en a un bien reçu sinon envoie un nack mais comme non traité dans l'emetteur : attend l'expiration du temporisateur
+            if (!verifier_controle(&paquet)){
+                printf("[TRP] Erreur de somme de controle.\n");
+                printf("[GAB] J'ai reçu %d mais j'attendais %d du paquet %d\n", paquet.somme_ctrl, generer_controle(&paquet), paquet.num_seq);
+                return 0;
+            }
+            else{
+                printf("[TRP] Hors fenetre.\n");
+                printf("[GAB] J'ai reçu %d mais j'attendais %d\n", paquet.num_seq, borne_inf);
+            }
+            if (pack.type == ACK){ //envoie du dernier bon ack si il y en a un bien reçu sinon attend l'expiration du temporisateur
+                vers_reseau(&pack); 
+                printf("[GAB] J'envoie le dernier bon ack %d mais j'ai reçu %d\n", pack.num_seq, paquet.num_seq);
+            }
         }
         
     }
