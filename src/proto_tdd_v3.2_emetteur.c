e4c7f695525da562e416f633144d182dbed56390
Gabriel <49925197+Darksword333@users.noreply.github.com>
2025-03-26 12:15:33 +0000

    init 3.2

---
+++ src/proto_tdd_v3.2_emetteur.c
@@ -0,0 +1,21 @@
+/*************************************************************
+* proto_tdd_v3.2 -  émetteur                                 *
+* TRANSFERT DE DONNEES  v3.2                                 *
+*                                                            *
+* Protocole Go-Back-N     Fast Retransmit                    *
+*                                                            *
+* MAZET Gabriel                                              *
+**************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "application.h"
+#include "couche_transport.h"
+#include "services_reseau.h"
+
+/* =============================== */
+/* Programme principal - émetteur  */
+/* =============================== */
+int main(int argc, char* argv[]){
+    return 0;
+}
